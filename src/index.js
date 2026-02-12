837ea83e30352c8192b74f55d4b4783d23758644
Ritik-kumar045 <ritikkumarsingh647@gmai.com>
2026-02-13 00:59:24 +0530

    Initial backend commit

---
+++ src/index.js
@@ -0,0 +1,58 @@
+//require('dotenv').config({path :'./env'})
+import express from "express";
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+import { app } from "./app.js"; 
+
+
+
+dotenv.config({
+    path: './.env'
+})
+
+
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`o server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("MongoDB connection failed !!!:", err)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*import express from "express";
+const app = express();
+(async()=>{
+    try{
+       await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+       app.on("Error",(error)=>{
+        console.log("Error: ", error);
+        throw error
+       })
+
+       app.listen(process.env.PORT,()=>{
+        console.log(`App is listening on port ${process.env.PORT}`)
+       })
+    }catch(error){
+         console.error("Error: ", error)
+         throw err
+    }
+})()*/
